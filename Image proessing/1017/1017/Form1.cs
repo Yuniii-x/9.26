38db89e0a96dd075b08bec0101c93a51af028ade
Yuniii-x <135338024+Yuniii-x@users.noreply.github.com>
2024-10-17 18:46:50 +0800

    Add files via upload

---
+++ Image proessing/1017/1017/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1017
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Read & Create a new bitmap
+            Bitmap bmpOrg = new Bitmap("mjq.png");
+            Bitmap bmpGray = new Bitmap(bmpOrg.Width, bmpOrg.Height, PixelFormat.Format8bppIndexed);
+
+            unsafe // must set allow unsafe code in project, for direct access pixels
+            {
+                BitmapData bmpOrgData = bmpOrg.LockBits(new Rectangle(0, 0, bmpOrg.Width,bmpOrg.Height),ImageLockMode.ReadOnly, bmpOrg.PixelFormat);
+                BitmapData bmpGrayData = bmpGray.LockBits(new Rectangle(0, 0, bmpGray.Width, bmpGray.Height),
+                    ImageLockMode.WriteOnly, PixelFormat.Format8bppIndexed);
+                int bytesPerPixel = System.Drawing.Bitmap.GetPixelFormatSize(bmpOrg.PixelFormat) / 8; // get bytes per pixel
+                int nHeight = bmpOrgData.Height;
+                int nWidth = bmpOrgData.Width;
+
+            }
+        }
+    }
+}
---
+++ Image proessing/9.26.1/9.26.1/9.26.1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _9._26._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //Read & Create a new bitmap
+            Bitmap bmpOrg = new Bitmap("lena.jpg");
+            Bitmap bmpGray = new Bitmap(bmpOrg.Width, bmpOrg.Height, PixelFormat.Format24bppRgb);
+
+            int nHeight = bmpOrg.Height;  // get image width & height
+            int nWidth = bmpOrg.Width;
+
+            for (int y = 0; y < nHeight; y++)  // process each scan line
+            {
+                for(int x = 0; x < nWidth; x++)  //process each pixel in the Current scan line
+                {
+                    Color Cpix = bmpOrg.GetPixel(x, y);
+                    int GrayValue;  //the Gary value of a pixel
+
+                    GrayValue = (int)(Cpix.R *0.2999 + Cpix.G*0.587 + Cpix.B*0.114) ; //Convert BGR to gray
+                    bmpGray.SetPixel(x,y,Color.FromArgb(GrayValue, GrayValue,GrayValue));
+                }
+            }
+
+            pictureBox1.Image = bmpOrg;  //dispaly bitmaps
+            pictureBox2.Image = bmpGray;
+
+            bmpGray.Save("Gray24Lena.png", ImageFormat.Png);  //save the gray image
+        }
+    }
+}
